37d7c48a0f873de35e2e0e2a430661be8cd94662
Nucleartom <jacob.maarse@gmail.com>
2018-04-02 22:22:29 +0000

    knex exercise

---
+++ add_person.js
@@ -0,0 +1,25 @@
+// const pg = require("pg");
+const settings = require("./settings"); // settings.json
+const connectionID = {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  };
+
+var knex = require('knex')({
+    client: 'pg',
+    connection: connectionID
+  });
+  let firstname = process.argv[2];
+  let lastname = process.argv[3];
+  let dob = process.argv[4];
+  let toBeInserted = [{first_name: firstname, last_name: lastname, birthdate: dob}]
+  // console.log(connectionID, "to be inserted: ", toBeInserted);
+//   knex.select().from('famous_people').then(function(results) {
+//     console.log(results);
+//   })
+  
+  knex('famous_people').insert(toBeInserted).then(function(results) {
+      return;
+  })
