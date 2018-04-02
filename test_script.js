7e6986b254465ecf64e89432d9aec83ac68106ab
Nucleartom <jacob.maarse@gmail.com>
2018-04-02 21:30:29 +0000

    completed js that queries database

--- test_script.js
+++ test_script.js
@@ -2,23 +2,28 @@ const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
 const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+    user: settings.user,
+    password: settings.password,
+    database: settings.database,
+    host: settings.hostname,
+    port: settings.port,
+    ssl: settings.ssl
 });
 
 client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    console.log(result.rows[0].number); //output: 1
-    client.end();
-  });
+    if (err) throw error;  
+    let someId = process.argv[2];
+    client.query(`select * from famous_people where first_name = $1 OR last_name = $1;`, [someId], (err, results) => {
+        if (err) {
+            throw error;
+        } else {
+            results.rows.forEach(row => {
+                console.log(row)
+                for (column in row) {
+                    console.log(column, row[column]);
+                }
+            })
+        }
+        client.end();
+    });
 });
